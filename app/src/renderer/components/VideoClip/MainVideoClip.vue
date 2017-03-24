ba725bcb72e900fd52b04cc46caafdce66a307a6
joe <qwert30830@gmail.com>
2017-03-24 16:14:11 +0800

    hahahahahahaha

--- app/src/renderer/components/VideoClip/MainVideoClip.vue
+++ app/src/renderer/components/VideoClip/MainVideoClip.vue
@@ -4,57 +4,80 @@
     <router-link to="/addclip_video"><button class="btn btn-default">新增媒體</button></router-link>
     <hr/>
     <div class="panel-group" id="accordion" style="padding-right:2% ">
-      <div class="panel">
-        <div class="btn-group-default">
-            <button class=" btn btn-default " style="width: 95.5%;text-align:left;" type=" button " data-parent="#accordion" data-toggle="collapse" href="#collapseTT" v>
+      <div class="panel" style="border:solid 1px lightgray;border-radius:5px">
+        <div class="btn-group-default" style="border:none">
+            <span class=" btn btn-default " style="width: 95.87%;text-align:left; border:none" type=" button " data-parent="#accordion" data-toggle="collapse" href="#collapseTT" v>
               <span type="button " class="close pull-left " style="margin-right:3%">&times&nbsp</span>
-              Hello
+              Nemo
               <span class="badge" style="float:right">15:30</span>
-            </button>
+            </span>
             <div class="dropdown" style="float:right;">
-              <span data-toggle="dropdown" class="btn btn-default pull-right glyphicon glyphicon-menu-hamburger" type="button"></span>
+              <span data-toggle="dropdown" class="btn btn-default pull-right glyphicon glyphicon-menu-hamburger" style="border:none;border-right:10px" type="button"></span>
               <ul class="dropdown-menu pull-right">
                   <li><a href="#" class = "glyphicon glyphicon-expand">&nbsp新增到播放列表</a></li>
                   <li><a href="#" class = "glyphicon glyphicon-pencil">&nbsp編輯</a></li>
               </ul>
             </div>
         </div>
-        <div id="collapseTT" class="collapse" style="width:95.5%;">
-            <ul class="list-group ">
+        <div id="collapseTT" class="collapse">
+            <ul class="list-group" style="margin-bottom:0px">
                 <li class="list-group-item " style="padding-left:6% "><a>English</a></li>
                 <li class="list-group-item " style="padding-left:6% "><a>Spanish</a></li>
                 <li class="list-group-item " style="padding-left:6% "><a>Taiwanese</a></li>
             </ul>
         </div>
     </div>
-        <div class="panel panel-default">
-            <h4 class="panel-title">
-                    <button type="button" class="list-group-item" data-parent="#accordion" data-toggle="collapse" href="#collapseCwo">
-                          <label style="padding-top:0.3%">Test</label>
-                          <span type = "button"  class="close pull-left" style="margin-right:2%">&times&nbsp</span>
-                          <span type = "button"  class="close pull-right" style="margin-left:2%"  data-toggle="dropdown">···</span>
-                          <ul class="dropdown-menu pull-right" role="menu" >
-                            <li><a>Add to PlayList</a></li>
-                            <li><a href="#">Another action</a></li>
-                            <li><a href="#">Something else here</a></li>
-                            <li class="divider"></li>
-                            <li><a href="#">Separated link</a></li>
-                          </ul>
-                          <span class="badge">5:30</span>
-                    </button>
 
-              </h4>
-            <div id="collapseCwo" class="panel-collapse collapse">
-                <ul class="list-group">
-                    <li class="list-group-item" style="padding-left:6%"><a>English</a></li>
-                    <li class="list-group-item" style="padding-left:6%"><a>Spanish</a></li>
-                    <li class="list-group-item" style="padding-left:6%"><a>Taiwanese</a></li>
-                </ul>
-            </div>
-        </div>
+    <div class="panel" style="border:solid 1px lightgray;border-radius:5px">
+      <div class="btn-group-default" style="border:none">
+          <span class=" btn btn-default " style="width: 95.87%;text-align:left; border:none" type=" button " data-parent="#accordion" data-toggle="collapse" href="#collapse2" v>
+            <span type="button " class="close pull-left " style="margin-right:3%">&times&nbsp</span>
+            Let it go
+            <span class="badge" style="float:right">5:30</span>
+          </span>
+          <div class="dropdown" style="float:right;">
+            <span data-toggle="dropdown" class="btn btn-default pull-right glyphicon glyphicon-menu-hamburger" style="border:none;border-right:10px" type="button"></span>
+            <ul class="dropdown-menu pull-right">
+                <li><a href="#" class = "glyphicon glyphicon-expand">&nbsp新增到播放列表</a></li>
+                <li><a href="#" class = "glyphicon glyphicon-pencil">&nbsp編輯</a></li>
+            </ul>
+          </div>
       </div>
-    </div>
+      <div id="collapse2" class="collapse">
+          <ul class="list-group" style="margin-bottom:0px">
+              <li class="list-group-item " style="padding-left:6% "><a>English</a></li>
+              <li class="list-group-item " style="padding-left:6% "><a>Spanish</a></li>
+              <li class="list-group-item " style="padding-left:6% "><a>Taiwanese</a></li>
+          </ul>
+      </div>
+  </div>
 
+  <div class="panel" style="border:solid 1px lightgray;border-radius:5px">
+    <div class="btn-group-default" style="border:none">
+        <span class=" btn btn-default " style="width: 95.87%;text-align:left; border:none" type=" button " data-parent="#accordion" data-toggle="collapse" href="#collapse3" v>
+          <span type="button " class="close pull-left " style="margin-right:3%">&times&nbsp</span>
+          Zoo
+          <span class="badge" style="float:right">12:30</span>
+        </span>
+        <div class="dropdown" style="float:right;">
+          <span data-toggle="dropdown" class="btn btn-default pull-right glyphicon glyphicon-menu-hamburger" style="border:none;border-right:10px" type="button"></span>
+          <ul class="dropdown-menu pull-right">
+              <li><a href="#" class = "glyphicon glyphicon-expand">&nbsp新增到播放列表</a></li>
+              <li><a href="#" class = "glyphicon glyphicon-pencil">&nbsp編輯</a></li>
+          </ul>
+        </div>
+    </div>
+    <div id="collapse3" class="collapse">
+        <ul class="list-group" style="margin-bottom:0px">
+            <li class="list-group-item " style="padding-left:6% "><a>English</a></li>
+            <li class="list-group-item " style="padding-left:6% "><a>Spanish</a></li>
+            <li class="list-group-item " style="padding-left:6% "><a>Taiwanese</a></li>
+        </ul>
+    </div>
+</div>
+
+
+    </div>
 </div>
 </template>
 
